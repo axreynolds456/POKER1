0a694c0d6082ab4033e72c82b3943ce01ca4f5c9
axreynolds456 <aidanxreynolds@gmail.com>
2024-04-14 22:45:55 -0700

    final change

--- src/players/AidanPlayer.java
+++ src/players/AidanPlayer.java
@@ -10,37 +10,37 @@ public class AidanPlayer extends Player {
     Random factor = new Random();
 
     boolean hasDecentHand;
+
     public AidanPlayer(String name) {
         super(name);
     }
-
 
 
     @Override
     protected void takePlayerTurn() {
         if (shouldFold()) {
             fold();
-        } else if (shouldRaise()) {
-            if (getGameState().getNumRoundStage() == 0){
-                double randomFactor = 1.0 + factor.nextDouble() * (maxRaiseMultiplier - 16.0); // Random factor between 1.0 and maxRaiseMultiplier
-                raise((int) (getGameState().getTableMinBet() * randomFactor));
-            }
-            if (getGameState().getNumRoundStage() == 1){
-                double randomFactor = 1.0 + factor.nextDouble() * (maxRaiseMultiplier - 12.0); // Random factor between 1.0 and maxRaiseMultiplier
-                raise((int) (getGameState().getTableMinBet() * randomFactor));
-            }
-            if (getGameState().getNumRoundStage() == 2){
-                double randomFactor = 1.0 + factor.nextDouble() * (maxRaiseMultiplier - 8.0); // Random factor between 1.0 and maxRaiseMultiplier
-                raise((int) (getGameState().getTableMinBet() * randomFactor));
-            }
-            if (getGameState().getNumRoundStage() == 3){
-                double randomFactor = 1.0 + factor.nextDouble() * (maxRaiseMultiplier - 4.0); // Random factor between 1.0 and maxRaiseMultiplier
-                raise((int) (getGameState().getTableMinBet() * randomFactor));
-            }
-        } else if (shouldCall()) {
+        }  else if (shouldCall()) {
             call();
         } else if (shouldCheck()) {
             check();
+        } else if (shouldRaise()) {
+            if (getGameState().getNumRoundStage() == 0) {
+                double randomFactor = 1.0 + factor.nextDouble() * (maxRaiseMultiplier - 16.0); // Random factor between 1.0 and maxRaiseMultiplier
+                raise((int) (getGameState().getTableMinBet() * randomFactor));
+            }
+            if (getGameState().getNumRoundStage() == 1) {
+                double randomFactor = 1.0 + factor.nextDouble() * (maxRaiseMultiplier - 12.0); // Random factor between 1.0 and maxRaiseMultiplier
+                raise((int) (getGameState().getTableMinBet() * randomFactor));
+            }
+            if (getGameState().getNumRoundStage() == 2) {
+                double randomFactor = 1.0 + factor.nextDouble() * (maxRaiseMultiplier - 8.0); // Random factor between 1.0 and maxRaiseMultiplier
+                raise((int) (getGameState().getTableMinBet() * randomFactor));
+            }
+            if (getGameState().getNumRoundStage() == 3) {
+                double randomFactor = 1.0 + factor.nextDouble() * (maxRaiseMultiplier - 4.0); // Random factor between 1.0 and maxRaiseMultiplier
+                raise((int) (getGameState().getTableMinBet() * randomFactor));
+            }
         } else if (shouldAllIn()) {
             allIn();
         }
@@ -49,8 +49,11 @@ public class AidanPlayer extends Player {
     @Override
     protected boolean shouldFold() {
         //if I have a pair midway through the game I fold and if the pot is 50% of my bank without a good hand
-        if((hasDecentHand == evaluatePlayerHand().getValue() <= HandRanks.PAIR.getValue() && (getGameState().getNumRoundStage() == 2)) || (hasDecentHand == evaluatePlayerHand().getValue() <= HandRanks.TWO_PAIR.getValue() && getGameState().getTableBet() >= getBank() * 0.5)) {
+        if ((hasDecentHand == evaluatePlayerHand().getValue() <= HandRanks.PAIR.getValue() && (getGameState().getNumRoundStage() == 2)) || (hasDecentHand == evaluatePlayerHand().getValue() <= HandRanks.TWO_PAIR.getValue() && getGameState().getTableBet() >= getBank() * 0.5)) {
             return true;
+        }
+        else if (hasDecentHand == evaluatePlayerHand().getValue() >= HandRanks.THREE_OF_A_KIND.getValue()) {
+            return false;
         }
         return false;
     }
@@ -58,7 +61,7 @@ public class AidanPlayer extends Player {
     @Override
     protected boolean shouldCheck() {
         // if there is no bet and I have less than a pair I check
-        if(!getGameState().isActiveBet() || hasDecentHand == evaluatePlayerHand().getValue() <= HandRanks.PAIR.getValue()) {
+        if (!getGameState().isActiveBet() || hasDecentHand == evaluatePlayerHand().getValue() <= HandRanks.PAIR.getValue()) {
             return true;
         }
         return false;
@@ -67,13 +70,13 @@ public class AidanPlayer extends Player {
     @Override
     protected boolean shouldCall() {
         // checks if there is an active bet and that it is less than 20% of my current bank
-        if(getGameState().isActiveBet() && getGameState().getTableBet() < getBank() * 0.2) {
+        if (getGameState().isActiveBet() && getGameState().getTableBet() < getBank() * 0.2) {
             // if I have a full house midway through the game or if I have a 2 pair at the beginning of the game
-            if((hasDecentHand == evaluatePlayerHand().getValue() <= HandRanks.FULL_HOUSE.getValue() && getGameState().getNumRoundStage() == 2) || (hasDecentHand == evaluatePlayerHand().getValue() <= HandRanks.TWO_PAIR.getValue() && getGameState().getNumRoundStage() == 1)) {
+            if ((hasDecentHand == evaluatePlayerHand().getValue() <= HandRanks.FULL_HOUSE.getValue() && getGameState().getNumRoundStage() == 2) || (hasDecentHand == evaluatePlayerHand().getValue() <= HandRanks.TWO_PAIR.getValue() && getGameState().getNumRoundStage() == 1)) {
                 shouldRaise();
             }
             // if I have less than a 2 pair at any stage, I check
-            else if(hasDecentHand == evaluatePlayerHand().getValue() >= HandRanks.TWO_PAIR.getValue()) {
+            else if (hasDecentHand == evaluatePlayerHand().getValue() >= HandRanks.TWO_PAIR.getValue()) {
                 return true;
             }
         }
@@ -85,8 +88,11 @@ public class AidanPlayer extends Player {
         if(hasDecentHand == evaluatePlayerHand().getValue() >= HandRanks.TWO_PAIR.getValue()) {
             return true;
         }
+        else {
             return false;
         }
+    }
+
 
     @Override
     protected boolean shouldAllIn() {
