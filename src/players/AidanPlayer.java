3bfa8a9226ec3462bba094d46b3649e665b56e9c
axreynolds456 <aidanxreynolds@gmail.com>
2024-04-14 23:14:00 -0700

    idea while I was going to bed

--- src/players/AidanPlayer.java
+++ src/players/AidanPlayer.java
@@ -20,10 +20,10 @@ public class AidanPlayer extends Player {
     protected void takePlayerTurn() {
         if (shouldFold()) {
             fold();
-        }  else if (shouldCall()) {
-            call();
         } else if (shouldCheck()) {
             check();
+        } else if (shouldCall()) {
+            call();
         } else if (shouldRaise()) {
             if (getGameState().getNumRoundStage() == 0) {
                 double randomFactor = 20.0 + factor.nextDouble() * (maxRaiseMultiplier - 12.0); // Random factor between 20.0 and maxRaiseMultiplier
@@ -61,7 +61,7 @@ public class AidanPlayer extends Player {
     @Override
     protected boolean shouldCheck() {
         // if there is no bet and I have less than a pair I check
-        if (!getGameState().isActiveBet() || hasDecentHand == evaluatePlayerHand().getValue() <= HandRanks.PAIR.getValue()) {
+        if (!getGameState().isActiveBet() || hasDecentHand == evaluatePlayerHand().getValue() <= HandRanks.HIGH_CARD.getValue()) {
             return true;
         }
         return false;
@@ -71,14 +71,7 @@ public class AidanPlayer extends Player {
     protected boolean shouldCall() {
         // checks if there is an active bet and that it is less than 20% of my current bank
         if (getGameState().isActiveBet() && getGameState().getTableBet() < getBank() * 0.2) {
-            // if I have a full house midway through the game or if I have a 2 pair at the beginning of the game
-            if ((hasDecentHand == evaluatePlayerHand().getValue() <= HandRanks.FULL_HOUSE.getValue() && getGameState().getNumRoundStage() == 2) || (hasDecentHand == evaluatePlayerHand().getValue() <= HandRanks.TWO_PAIR.getValue() && getGameState().getNumRoundStage() == 1)) {
-                shouldRaise();
-            }
-            // if I have less than a 2 pair at any stage, I check
-            else if (hasDecentHand == evaluatePlayerHand().getValue() >= HandRanks.TWO_PAIR.getValue()) {
-                return true;
-            }
+            return true;
         }
         return false;
     }
